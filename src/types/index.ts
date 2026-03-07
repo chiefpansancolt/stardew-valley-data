5c586fdc931d9b00ab19662debfb01631594f6da
Christopher Pezza <chris.pezza@hey.com>
2026-03-06 21:09:00 -0500

    add tree data

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './common';
 export * from './crop';
 export * from './mixed-seed';
+export * from './tree';
---
+++ src/types/tree.ts
@@ -0,0 +1,55 @@
+import { Season } from './common';
+import { EnergyHealth, SeedBuyPrice } from './crop';
+
+export interface TreeStage {
+  name: string;
+  image: string;
+}
+
+export interface FruitTreeProduce {
+  id: string;
+  name: string;
+  sellPrice: number;
+  image: string;
+  energyHealth?: EnergyHealth;
+}
+
+export interface FruitTree {
+  type: 'fruit-tree';
+  id: string;
+  name: string;
+  saplingId: string;
+  saplingName: string;
+  saplingBuyPrices: SeedBuyPrice[];
+  saplingSellPrice: number;
+  seasons: Season[];
+  daysToMature: number;
+  description: string;
+  image: string;
+  saplingImage: string;
+  stages: TreeStage[];
+  produce: FruitTreeProduce;
+}
+
+export interface WildTreeTapper {
+  id: string;
+  name: string;
+  sellPrice: number;
+  image: string;
+  energyHealth?: EnergyHealth;
+}
+
+export interface WildTree {
+  type: 'wild-tree';
+  id: string;
+  name: string;
+  seedId: string;
+  seedName: string;
+  description: string;
+  image: string;
+  seedImage: string;
+  stages: TreeStage[];
+  tapper?: WildTreeTapper;
+}
+
+export type Tree = FruitTree | WildTree;
